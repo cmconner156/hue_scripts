f5ceb85ff367a732e103e04f4ec81f0e3dd7cdcf
Chris Conner <cconner@cloudera.com>
2016-01-11 11:09:09 -0500

    Updated the hue_history_cron to have options to only clear beeswax or oozie tables.

---
+++ hue_create_db.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+PARCEL_DIR=/opt/cloudera/parcels/CDH
+LOG_FILE=/var/log/hue/`basename "$0" | awk -F\. '{print $1}'`.log
+DATABASE=$1
+PASSWORD=$2
+
+if [[ -z ${PASSWORD} ]]
+then
+   PASSWORD="password"
+fi
+
+if [[ -z ${DATABASE} ]]
+then
+   echo "Usage: hue_create_db.sh <database_name> <password>"
+   exit 1
+fi
+
+HUE_CONF_DIR=/tmp/hue_create_db/${DATABASE}
+mkdir -p ${HUE_CONF_DIR}
+
+if [ ! -d "/usr/lib/hadoop" ]
+then
+   CDH_HOME=$PARCEL_DIR
+else
+   CDH_HOME=/usr
+fi
+
+if [ -d "${CDH_HOME}/lib/hue/build/env/bin" ]
+then
+   COMMAND="${CDH_HOME}/lib/hue/build/env/bin/hue"
+else
+   COMMAND="${CDH_HOME}/share/hue/build/env/bin/hue"
+fi
+
+ORACLE_HOME=/opt/cloudera/parcels/ORACLE_INSTANT_CLIENT/instantclient_11_2/
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}
+export CDH_HOME HUE_CONF_DIR ORACLE_HOME LD_LIBRARY_PATH COMMAND PASSWORD DATABASE
+
+cat > ${HUE_CONF_DIR}/hue.ini << EOF
+[desktop]
+[[database]]
+#engine=sqlite3
+#name=/var/lib/hue/desktop.db
+engine=mysql
+host=`hostname`
+port=3306
+user=hue
+password=${PASSWORD}
+name=${DATABASE}
+EOF
+
+cat > ${HUE_CONF_DIR}/create.sql << EOF
+drop database if exists ${DATABASE};
+create database ${DATABASE};
+EOF
+
+mysql -uroot -p${PASSWORD} < ${HUE_CONF_DIR}/create.sql
+
+${COMMAND} syncdb --noinput
+${COMMAND} migrate --merge
+
+cat > ${HUE_CONF_DIR}/prepare.sql << EOF
+ALTER TABLE auth_permission DROP FOREIGN KEY content_type_id_refs_id_728de91f;
+delete from django_content_type;
+EOF
+
+mysql -uroot -p${PASSWORD} ${DATABASE} < ${HUE_CONF_DIR}/prepare.sql
+
+
+
+
--- hue_history_cron.sh
+++ hue_history_cron.sh
@@ -17,43 +17,111 @@
 #
 #Cleans up old oozie workflow and beeswax savedqueries to
 #prevent the DB from getting too large.
-PARCEL_DIR=/opt/cloudera/parcels/CDH
-LOG_FILE=/var/log/hue/`basename "$0" | awk -F\. '{print $1}'`.log
-LOG_ROTATE_SIZE=10 #MB before rotating, size in MB before rotating log to .1
-LOG_ROTATE_COUNT=2 #number of log files, so 20MB max
-DATE=`date '+%Y%m%d-%H%M'`
-KEEP_DAYS=7    #Number of days of beeswax and oozie history to keep
-BEESWAX_DELETE_RECORDS=999 #number of beeswax records to delete at a time
-                             #to avoid Non Fatal Exception: DatabaseError: too many SQL variables
-WORKFLOW_DELETE_RECORDS=999 #number of workflow records to delete at a time
-                             #to avoid Non Fatal Exception: DatabaseError: too many SQL variables
-RESET_COUNT=15              #number of deletion attempts before trying max again
-RESET_MAX=5                 #number of resets permitted
 
-if [ ! -d "/usr/lib/hadoop" ]
-then
-   CDH_HOME=$PARCEL_DIR
-else
-   CDH_HOME=/usr
-fi
 
-if [ -d "/var/run/cloudera-scm-agent/process" ]
-then
-   HUE_CONF_DIR="/var/run/cloudera-scm-agent/process/`ls -1 /var/run/cloudera-scm-agent/process | grep HUE | sort -n | tail -1 `"
-else
-   HUE_CONF_DIR="/etc/hue/conf"
-fi
+#parse command line arguments
+parse_arguments()
+{
+  # Test that we're using compatible getopt version.
+  getopt -T > /dev/null
+  if [[ $? -ne 4 ]]; then
+    echo "Incompatible getopt version."
+    exit 1
+  fi
 
-if [ -d "${CDH_HOME}/lib/hue/build/env/bin" ]
-then
-   COMMAND="${CDH_HOME}/lib/hue/build/env/bin/hue shell"
-else
-   COMMAND="${CDH_HOME}/share/hue/build/env/bin/hue shell"
-fi
+  # Parse short and long option parameters.
+  BEESWAX=true
+  OOZIE=true
+  GETOPT=`getopt -n $0 -o b,o,h \
+      -l beeswax,oozie,help \
+      -- "$@"`
+  eval set -- "$GETOPT"
+  while true;
+  do
+    case "$1" in
+    -b|--nobeeswax)
+      BEESWAX=
+      shift
+      ;;
+    -o|--nooozie)
+      OOZIE=
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+    esac
+  done
+  #
+}
 
-ORACLE_HOME=/opt/cloudera/parcels/ORACLE_INSTANT_CLIENT/instantclient_11_2/
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}
-export CDH_HOME HUE_CONF_DIR ORACLE_HOME LD_LIBRARY_PATH COMMAND DEBUG=true DESKTOP_DEBUG=true
+usage()
+{
+cat << EOF
+usage: $0 [options]
+
+Cleans up the oozie_job, beeswax_queryhistory, beeswax_savedquery tables:
+
+OPTIONS
+   -b|--nobeeswax          Disables cleaning of beeswax tables.
+   -o|--nooozie            Disables cleaning of the oozie tables.
+   -h|--help               Show this message.
+EOF
+}
+
+main()
+{
+
+   parse_arguments "$@"
+
+   if [[ ! ${USER} =~ .*root* ]]
+   then
+      echo "Script must be run as root: exiting"
+      exit 1
+   fi
+
+   PARCEL_DIR=/opt/cloudera/parcels/CDH
+   LOG_FILE=/var/log/hue/`basename "$0" | awk -F\. '{print $1}'`.log
+   LOG_ROTATE_SIZE=10 #MB before rotating, size in MB before rotating log to .1
+   LOG_ROTATE_COUNT=2 #number of log files, so 20MB max
+   DATE=`date '+%Y%m%d-%H%M'`
+   KEEP_DAYS=7    #Number of days of beeswax and oozie history to keep
+   BEESWAX_DELETE_RECORDS=999 #number of beeswax records to delete at a time
+                                #to avoid Non Fatal Exception: DatabaseError: too many SQL variables
+   WORKFLOW_DELETE_RECORDS=999 #number of workflow records to delete at a time
+                                #to avoid Non Fatal Exception: DatabaseError: too many SQL variables
+   RESET_COUNT=15              #number of deletion attempts before trying max again
+   RESET_MAX=5                 #number of resets permitted
+
+   if [ ! -d "/usr/lib/hadoop" ]
+   then
+      CDH_HOME=$PARCEL_DIR
+   else
+      CDH_HOME=/usr
+   fi
+
+   if [ -d "/var/run/cloudera-scm-agent/process" ]
+   then
+      HUE_CONF_DIR="/var/run/cloudera-scm-agent/process/`ls -1 /var/run/cloudera-scm-agent/process | grep HUE | sort -n | tail -1 `"
+   else
+      HUE_CONF_DIR="/etc/hue/conf"
+   fi
+
+   if [ -d "${CDH_HOME}/lib/hue/build/env/bin" ]
+   then
+      COMMAND="${CDH_HOME}/lib/hue/build/env/bin/hue shell"
+   else
+      COMMAND="${CDH_HOME}/share/hue/build/env/bin/hue shell"
+   fi
+
+   ORACLE_HOME=/opt/cloudera/parcels/ORACLE_INSTANT_CLIENT/instantclient_11_2/
+   LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}
+   export CDH_HOME HUE_CONF_DIR ORACLE_HOME LD_LIBRARY_PATH COMMAND DEBUG=true DESKTOP_DEBUG=true
 
 ${COMMAND} >> /dev/null 2>&1 <<EOF
 from beeswax.models import SavedQuery
@@ -84,60 +152,65 @@ log.addHandler(fh)
 log.info('HUE_CONF_DIR: ${HUE_CONF_DIR}')
 log.info("Cleaning up anything in the Hue tables oozie*, desktop* and beeswax* older than ${KEEP_DAYS} old")
 
-totalQuerys = SavedQuery.objects.filter(is_auto=True, mtime__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)
-log.info("Looping through querys. %s querys to be deleted." % totalQuerys.count())
-while totalQuerys.count():
-   if deleteBeeswaxRecords < 30 and resets < resetMax:
-      checkCount += 1
-   if checkCount == resetCount:
-      deleteBeeswaxRecords = ${BEESWAX_DELETE_RECORDS}
-      resets += 1
-      checkCount = 0
-   log.info("SavedQuerys left: %s" % totalQuerys.count())
-   savedQuerys = SavedQuery.objects.filter(is_auto=True, mtime__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)[:deleteBeeswaxRecords]
-   try:
-      SavedQuery.objects.filter(pk__in = savedQuerys).delete()
-      errorCount = 0
-   except DatabaseError, e:
-      log.info("Non Fatal Exception: %s: %s" % (e.__class__.__name__, e))
-      errorCount += 1
-      if errorCount > 9 and deleteBeeswaxRecords == 1:
-         raise
-      if deleteBeeswaxRecords > 100:
-         deleteBeeswaxRecords = max(deleteBeeswaxRecords - 100, 1)
-      else:
-         deleteBeeswaxRecords = max(deleteBeeswaxRecords - 10, 1)
-      log.info("Decreasing max delete records for SavedQuerys to: %s" % deleteBeeswaxRecords)
+if "${BEESWAX}" == "true":
    totalQuerys = SavedQuery.objects.filter(is_auto=True, mtime__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)
+   log.info("Looping through querys. %s querys to be deleted." % totalQuerys.count())
+   while totalQuerys.count():
+      if deleteBeeswaxRecords < 30 and resets < resetMax:
+         checkCount += 1
+      if checkCount == resetCount:
+         deleteBeeswaxRecords = ${BEESWAX_DELETE_RECORDS}
+         resets += 1
+         checkCount = 0
+      log.info("SavedQuerys left: %s" % totalQuerys.count())
+      savedQuerys = SavedQuery.objects.filter(is_auto=True, mtime__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)[:deleteBeeswaxRecords]
+      try:
+         SavedQuery.objects.filter(pk__in = savedQuerys).delete()
+         errorCount = 0
+      except DatabaseError, e:
+         log.info("Non Fatal Exception: %s: %s" % (e.__class__.__name__, e))
+         errorCount += 1
+         if errorCount > 9 and deleteBeeswaxRecords == 1:
+            raise
+         if deleteBeeswaxRecords > 100:
+            deleteBeeswaxRecords = max(deleteBeeswaxRecords - 100, 1)
+         else:
+            deleteBeeswaxRecords = max(deleteBeeswaxRecords - 10, 1)
+         log.info("Decreasing max delete records for SavedQuerys to: %s" % deleteBeeswaxRecords)
+      totalQuerys = SavedQuery.objects.filter(is_auto=True, mtime__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)
 
 errorCount = 0
 checkCount = 0
 resets = 0
-totalWorkflows = Workflow.objects.filter(is_trashed=True, last_modified__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)
-log.info("Looping through workflows. %s workflows to be deleted." % totalWorkflows.count())
-while totalWorkflows.count():
-   if deleteWorkflowRecords < 30 and resets < resetMax:
-      checkCount += 1
-   if checkCount == resetCount:
-      deleteWorkflowRecords = ${WORKFLOW_DELETE_RECORDS}
-      resets += 1
-      checkCount = 0
-   log.info("Workflows left: %s" % totalWorkflows.count())
-   deleteWorkflows = Workflow.objects.filter(is_trashed=True, last_modified__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)[:deleteWorkflowRecords]
-   try:
-      Workflow.objects.filter(pk__in = deleteWorkflows).delete()
-      errorCount = 0
-   except DatabaseError, e:
-      log.info("Non Fatal Exception: %s: %s" % (e.__class__.__name__, e))
-      errorCount += 1
-      if errorCount > 9 and deleteWorkflowRecords == 1:
-         raise
-      if deleteWorkflowRecords > 100:
-         deleteWorkflowRecords = max(deleteWorkflowRecords - 100, 1)
-      else:
-         deleteWorkflowRecords = max(deleteWorkflowRecords - 10, 1)
-      log.info("Decreasing max delete records for Workflows to: %s" % deleteWorkflowRecords)
+if "${OOZIE}" == "true":
    totalWorkflows = Workflow.objects.filter(is_trashed=True, last_modified__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)
+   log.info("Looping through workflows. %s workflows to be deleted." % totalWorkflows.count())
+   while totalWorkflows.count():
+      if deleteWorkflowRecords < 30 and resets < resetMax:
+         checkCount += 1
+      if checkCount == resetCount:
+         deleteWorkflowRecords = ${WORKFLOW_DELETE_RECORDS}
+         resets += 1
+         checkCount = 0
+      log.info("Workflows left: %s" % totalWorkflows.count())
+      deleteWorkflows = Workflow.objects.filter(is_trashed=True, last_modified__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)[:deleteWorkflowRecords]
+      try:
+         Workflow.objects.filter(pk__in = deleteWorkflows).delete()
+         errorCount = 0
+      except DatabaseError, e:
+         log.info("Non Fatal Exception: %s: %s" % (e.__class__.__name__, e))
+         errorCount += 1
+         if errorCount > 9 and deleteWorkflowRecords == 1:
+            raise
+         if deleteWorkflowRecords > 100:
+            deleteWorkflowRecords = max(deleteWorkflowRecords - 100, 1)
+         else:
+            deleteWorkflowRecords = max(deleteWorkflowRecords - 10, 1)
+         log.info("Decreasing max delete records for Workflows to: %s" % deleteWorkflowRecords)
+      totalWorkflows = Workflow.objects.filter(is_trashed=True, last_modified__lte=date.today() - timedelta(days=keepDays)).values_list("id", flat=True)
 
 EOF
 
+}
+
+main "$@"
