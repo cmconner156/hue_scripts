8ebaeb3969a6b3074e1137316d429a7b62efd38b
Chris Conner <cconner@cloudera.com>
2017-02-06 10:48:40 -0500

    Added another invalid doc entry

--- hue_migrate_queries.sh
+++ hue_migrate_queries.sh
@@ -60,7 +60,7 @@ from desktop.converters import DocumentConverter
 from beeswax.models import SavedQuery, HQL, IMPALA, RDBMS
 import re
 
-DOC2_NAME_INVALID_CHARS = "[<>/~\`]"
+DOC2_NAME_INVALID_CHARS = "[<>/~\`u'\xe9']" 
 
 count = 0
 for user in User.objects.filter():
