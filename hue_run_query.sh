c4c435e923b37c86cc8afdf5ff72c868f70333ef
Chris Conner <cconner@cloudera.com>
2017-05-30 17:41:34 -0400

    Fixed some stuff

--- hue_run_query.sh
+++ hue_run_query.sh
@@ -15,17 +15,17 @@ parse_arguments()
   OVERRIDE=
   VERBOSE=
   DESKTOP_DEBUG=
-  QUERY="select count(*) from default.sample_07"
+  TABLE="default.sample_07"
   USERNAME="admin"
-  GETOPT=`getopt -n $0 -o q:,u:o,v,h \
-      -l query:,username:,override,verbose,help \
+  GETOPT=`getopt -n $0 -o t:,u:o,v,h \
+      -l table:,username:,override,verbose,help \
       -- "$@"`
   eval set -- "$GETOPT"
   while true;
   do
     case "$1" in
-    -q|--query)
-      QUERY=$2
+    -t|--table)
+      TABLE=$2
       shift 2
       ;;
     -u|--username)
@@ -88,106 +88,127 @@ main()
   PYTHONPATH=${SCRIPT_DIR}/lib:${PYTHONPATH}
   export SCRIPT_DIR PYTHONPATH
 
-  #SET IMPORTANT ENV VARS
-  if [ -d "/var/run/cloudera-scm-agent/process" ]
-  then
-    if [[ -z ${HUE_CONF_DIR} ]]
+  rm -f /var/lib/hue/hue_run_query.end
+  while [ ! -f "/var/lib/hue/hue_run_query.end" ]
+  do
+  
+    #SET IMPORTANT ENV VARS
+    if [ -d "/var/run/cloudera-scm-agent/process" ]
     then
-      HUE_CONF_DIR="/var/run/cloudera-scm-agent/process/`ls -1 /var/run/cloudera-scm-agent/process | grep HUE_SERVER | sort -n | tail -1 `"
-    fi
-  else
-    HUE_CONF_DIR="/etc/hue/conf"
-  fi
-  HUE_SUPERVISOR_CONF=$(echo ${HUE_CONF_DIR} | sed "s/process/supervisor\/include/g").conf
-  export HUE_CONF_DIR HUE_SUPERVISOR_CONF
-
-  if [[ ! ${USER} =~ .*root* ]]
-  then
-    if [[ -z ${OVERRIDE} ]]
-    then
-      echo "Script must be run as root: exiting"
-      exit 1
-    fi
-  else
-    if [[ $(ps -ef | grep [r]unc) ]]
-    then
-      DESKTOP_LOG_DIR=$(strings /proc/$(ps -ef | grep [r]unc | awk '{print $2}')/environ | grep DESKTOP_LOG_DIR | awk -F\= '{print $2}')
-      export $(sed "s/,/\\n/g" ${HUE_SUPERVISOR_CONF} | grep DESKTOP_LOG_DIR | sed "s/'//g")
-    fi
-  fi
-
-  if [[ -z ${DESKTOP_LOG_DIR} ]]
-  then
-    DESKTOP_LOG_DIR=${HUE_CONF_DIR}/logs
-  fi
-  if [[ ! -f ${DESKTOP_LOG_DIR} ]]
-  then
-    mkdir -p ${DESKTOP_LOG_DIR}
-  fi
-  LOG_FILE=${DESKTOP_LOG_DIR}/`basename "$0" | awk -F\. '{print $1}'`.log
-  LOG_ROTATE_SIZE=10 #MB before rotating, size in MB before rotating log to .1
-  LOG_ROTATE_COUNT=5 #number of log files, so 20MB max
- 
-  if [ ! -d "/usr/lib/hadoop" ]
-  then
-    export $(sed "s/,/\\n/g" ${HUE_SUPERVISOR_CONF} | grep PARCELS_ROOT | sed "s/'//g")
-    PARCEL_DIR=${PARCELS_ROOT}/CDH
-    CDH_HOME=$PARCEL_DIR
-  else
-    CDH_HOME=/usr
-  fi
-
-  if [ -d "${CDH_HOME}/lib/hue/build/env/bin" ]
-  then
-    HUE_HOME="${CDH_HOME}/lib/hue"
-  else
-    HUE_HOME="${CDH_HOME}/share/hue"
-  fi
-  HUE_BIN="${HUE_HOME}/build/env/bin"
-  COMMAND="${HUE_BIN}/hue shell"
-  TEST_COMMAND="${HUE_BIN}/hue dbshell"
-  PYTHON="${HUE_BIN}/python"
-
-  ORACLE_ENGINE_CHECK=$(grep engine ${HUE_CONF_DIR}/hue* | grep -i oracle)
-  if [[ ! -z ${ORACLE_ENGINE_CHECK} ]]
-  then
-    if [[ -z ${ORACLE_HOME} ]]
-    then
-      ORACLE_PARCEL=/opt/cloudera/parcels/ORACLE_INSTANT_CLIENT/instantclient_11_2
-      if [[ -d ${ORACLE_PARCEL} ]]
+      if [[ -z ${HUE_CONF_DIR} ]]
       then
-        ORACLE_HOME=${ORACLE_PARCEL}
-        LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}
-        export LD_LIBRARY_PATH ORACLE_HOME
+        HUE_CONF_DIR="/var/run/cloudera-scm-agent/process/`ls -1 /var/run/cloudera-scm-agent/process | grep HUE_SERVER | sort -n | tail -1 `"
+      fi
+    else
+      HUE_CONF_DIR="/etc/hue/conf"
+    fi
+    HUE_SUPERVISOR_CONF=$(echo ${HUE_CONF_DIR} | sed "s/process/supervisor\/include/g").conf
+    export HUE_CONF_DIR HUE_SUPERVISOR_CONF
+
+    if [[ ! ${USER} =~ .*root* ]]
+    then
+      if [[ -z ${OVERRIDE} ]]
+      then
+        echo "Script must be run as root: exiting"
+        exit 1
+      fi
+    else
+      if [[ $(ps -ef | grep [r]unc) ]]
+      then
+        DESKTOP_LOG_DIR=$(strings /proc/$(ps -ef | grep [r]unc | awk '{print $2}')/environ | grep DESKTOP_LOG_DIR | awk -F\= '{print $2}')
+        export $(sed "s/,/\\n/g" ${HUE_SUPERVISOR_CONF} | grep DESKTOP_LOG_DIR | sed "s/'//g")
       fi
     fi
-    if [[ -z ${ORACLE_HOME} ]]
+
+    if [[ -z ${DESKTOP_LOG_DIR} ]]
     then
-      echo "It looks like you are using Oracle as your backend"
-      echo "ORACLE_HOME must be set to the correct Oracle client"
-      echo "before running this script"
+      DESKTOP_LOG_DIR=${HUE_CONF_DIR}/logs
+    fi
+    if [[ ! -f ${DESKTOP_LOG_DIR} ]]
+    then
+      mkdir -p ${DESKTOP_LOG_DIR}
+    fi
+    LOG_FILE=${DESKTOP_LOG_DIR}/`basename "$0" | awk -F\. '{print $1}'`
+    LOG_ROTATE_SIZE=10 #MB before rotating, size in MB before rotating log to .1
+    LOG_ROTATE_COUNT=5 #number of log files, so 20MB max
+ 
+    if [ ! -d "/usr/lib/hadoop" ]
+    then
+      export $(sed "s/,/\\n/g" ${HUE_SUPERVISOR_CONF} | grep PARCELS_ROOT | sed "s/'//g")
+      PARCEL_DIR=${PARCELS_ROOT}/CDH
+      CDH_HOME=$PARCEL_DIR
+    else
+      CDH_HOME=/usr
+    fi
+
+    if [ -d "${CDH_HOME}/lib/hue/build/env/bin" ]
+    then
+      HUE_HOME="${CDH_HOME}/lib/hue"
+    else
+      HUE_HOME="${CDH_HOME}/share/hue"
+    fi
+    HUE_BIN="${HUE_HOME}/build/env/bin"
+    COMMAND="${HUE_BIN}/hue shell"
+    TEST_COMMAND="${HUE_BIN}/hue dbshell"
+    PYTHON="${HUE_BIN}/python"
+
+    ORACLE_ENGINE_CHECK=$(grep engine ${HUE_CONF_DIR}/hue* | grep -i oracle)
+    if [[ ! -z ${ORACLE_ENGINE_CHECK} ]]
+    then
+      if [[ -z ${ORACLE_HOME} ]]
+      then
+        ORACLE_PARCEL=/opt/cloudera/parcels/ORACLE_INSTANT_CLIENT/instantclient_11_2
+        if [[ -d ${ORACLE_PARCEL} ]]
+        then
+          ORACLE_HOME=${ORACLE_PARCEL}
+          LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}
+          export LD_LIBRARY_PATH ORACLE_HOME
+        fi
+      fi
+      if [[ -z ${ORACLE_HOME} ]]
+      then
+        echo "It looks like you are using Oracle as your backend"
+        echo "ORACLE_HOME must be set to the correct Oracle client"
+        echo "before running this script"
+        exit 1
+      fi
+    fi
+
+    HUE_IGNORE_PASSWORD_SCRIPT_ERRORS=1
+    if [[ -z ${HUE_DATABASE_PASSWORD} ]]
+    then
+      echo "CDH 5.5 and above requires that you set the environment variable:"
+      echo "HUE_DATABASE_PASSWORD=<dbpassword>"
       exit 1
     fi
-  fi
+    PGPASSWORD=${HUE_DATABASE_PASSWORD}
+    export CDH_HOME COMMAND HUE_IGNORE_PASSWORD_SCRIPT_ERRORS PGPASSWORD
 
-  HUE_IGNORE_PASSWORD_SCRIPT_ERRORS=1
-  if [[ -z ${HUE_DATABASE_PASSWORD} ]]
-  then
-    echo "CDH 5.5 and above requires that you set the environment variable:"
-    echo "HUE_DATABASE_PASSWORD=<dbpassword>"
-    exit 1
-  fi
-  PGPASSWORD=${HUE_DATABASE_PASSWORD}
-  export CDH_HOME COMMAND HUE_IGNORE_PASSWORD_SCRIPT_ERRORS PGPASSWORD
+    while read -r ENV
+    do
+      export $ENV
+    done < <(grep environment ${HUE_SUPERVISOR_CONF} | awk -Fenvironment\= '{print $2}' | sed "s/,/\\n/g" | grep -v CM_STATUS | sed "s/'//g")
 
-  while read -r ENV
-  do
-    export $ENV
-  done < <(grep environment ${HUE_SUPERVISOR_CONF} | awk -Fenvironment\= '{print $2}' | sed "s/,/\\n/g" | grep -v CM_STATUS | sed "s/'//g")
-  sudo -E -u hue /bin/bash -c "DESKTOP_DEBUG=true ${PYTHON} ${SCRIPT_DIR}/hue_run_query.py ${HUE_HOME} ${USERNAME} ${QUERY}"
 
-  echo ""
-  echo "Logs can be found in ${DESKTOP_LOG_DIR}"
+    DATE=$(date '+%Y%m%d-%H%M%S')
+    PID=$(ps -ef | grep [r]unc | awk '{print $2}')
+    top -b -n 1 -u hue > ${DESKTOP_LOG_DIR}/top_${DATE}.log
+
+    netstat -anp | grep ${PID} >> ${DESKTOP_LOG_DIR}/netstat_${DATE}.log
+
+    timeout 15s /usr/bin/strace -f -v -p ${PID} -o ${DESKTOP_LOG_DIR}/strace_${DATE}.log -T -t &
+
+    LSOF=$(which lsof)
+    if [ -f ${LSOF} ]
+    then
+      ${LSOF} -P -p ${PID} ${ARGS} >> ${DESKTOP_LOG_DIR}/lsof_${DATE}.log
+    fi
+
+    sudo -E -u hue /bin/bash -c "DESKTOP_DEBUG=true ${PYTHON} ${SCRIPT_DIR}/hue_run_query.py ${LOG_FILE}_${DATE} ${HUE_HOME} ${USERNAME} '${TABLE}'" > /dev/null 2>&1
+
+    sleep 300
+
+  done
 
   unset PGPASSWORD
 
