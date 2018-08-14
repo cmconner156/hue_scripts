8d55d4a9f2f3a1194475196a1a6d60e06e49c840
Chris Conner <cconner@cloudera.com>
2018-08-14 09:22:23 -0400

    Fixed a bug

--- lib/custom_commands/management/commands/remove_doc2_without_content_object.py
+++ lib/custom_commands/management/commands/remove_doc2_without_content_object.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext_lazy as _t, ugettext as _
