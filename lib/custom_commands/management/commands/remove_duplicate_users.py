7cd926c096ea2bfa3ad514686ed948a440f526d4
Chris Conner <cconner@cloudera.com>
2018-10-17 10:17:29 -0400

    Minor fixes

--- lib/custom_commands/management/commands/remove_duplicate_users.py
+++ lib/custom_commands/management/commands/remove_duplicate_users.py
@@ -24,9 +24,21 @@ class Command(BaseCommand):
     new_username = rename_user + "renamed"
     LOG.warn("%s is newer than %s, renaming %s to %s" % (rename_user, keep_user, rename_user, new_username))
     self.users_dict[self.username][count]["username"] = new_username
-#    user_mod = User.objects.get(username=rename_user)
-#    user_mod.username = new_username
-#    user_mod.save()
+    user_mod = User.objects.get(username=rename_user)
+    user_mod.username = new_username
+    user_mod.save()
+
+
+  def log_users_list(self):
+    users_unsorted = []
+    for user in User.objects.filter():
+      users_unsorted.append(user.username)
+
+    users_sorted = sorted(users_unsorted, key=lambda s: s.lower())
+    count = 0
+    while count < len(users_sorted):
+      LOG.warn("%s" % users_sorted[count])
+      count = count + 1
 
 
   def handle(self, *args, **options):
@@ -41,14 +53,16 @@ class Command(BaseCommand):
     self.users_dict = {}
     self.username = None
 
+    LOG.warn("users list before renames")
+    self.log_users_list()
+
+
     for user in User.objects.filter():
       user_list = [{'username': user.username, 'date_joined': user.date_joined, 'date_joined_readable': user.date_joined.strftime('%Y-%m-%d %H:%M:%S%z')}]
       self.users_dict[user.username.lower()] = user_list
       for usercompare in User.objects.filter():
         if usercompare.id != user.id and usercompare.username.lower() == user.username.lower():
           self.users_dict[user.username.lower()].append({'username': usercompare.username, 'date_joined': usercompare.date_joined, 'date_joined_readable': usercompare.date_joined.strftime('%Y-%m-%d %H:%M:%S%z')})
-
-    LOG.warn("users_dict before update: %s" % pformat(self.users_dict, indent=4))
 
     for username in self.users_dict.keys():
       self.username = username
@@ -62,7 +76,6 @@ class Command(BaseCommand):
           date1 = current_dict[count]['date_joined']
           username2 = current_dict[count + 1]['username']
           date2 = current_dict[count + 1]['date_joined']
-          LOG.warn("Processing: count: %s: base: %s: with u1 %s : u2 %s" % (count, self.username, username1, username2))
           if date1 < date2:
             oldest_user = username1
             oldest_date = date1
@@ -76,14 +89,10 @@ class Command(BaseCommand):
         else:
           username2 = current_dict[count]['username']
           date2 = current_dict[count]['date_joined']
-          LOG.warn("Processing: count: %s: base: %s: with oldest_user %s : u2 %s" % (count, self.username, oldest_user, username2))
           if username2.lower() == self.username and username2 != oldest_user:
-            LOG.warn("if username2.lower() == self.username and username2 != oldest")
             if oldest_date < date2:
-              LOG.warn("if oldest_date < date2")
               self.rename_user(rename_user=username2, keep_user=oldest_user, count=count)
             else:
-              LOG.warn("else oldest_date < date2")
               self.rename_user(rename_user=oldest_user, keep_user=username2, count=oldest_count)
               oldest_user = username2
               oldest_date = date2
@@ -91,7 +100,9 @@ class Command(BaseCommand):
 
         count = count + 1
 
-    LOG.warn("users_dict after update: %s" % pformat(self.users_dict, indent=4))
+
+    LOG.warn("users list after renames")
+    self.log_users_list()
 
     #        transaction.commit()
 
