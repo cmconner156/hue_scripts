d1ddf3303ee8a52a1aa107f01fe47bf74a2f0dae
cconner <cconner@cloudera.com>
2019-04-24 10:44:12 -0400

    Updated stuff

--- lib/custom_commands/management/commands/remove_orphaned_docs.py
+++ lib/custom_commands/management/commands/remove_orphaned_docs.py
@@ -71,6 +71,8 @@ class Command(BaseCommand):
       if doc.type == "oozie-workflow2":
         name = doc.name
         doc2 = doc.copy(name=name, owner=docstorage)
+        print docstorage.__dict__
+        print doc2.__dict__
         print "migrating workflow: %s : %s : %s : %s : to user: %s" % (doc2.name, doc2.type, doc2.owner_id, doc2.parent_directory, docstorage_id)
 
 
