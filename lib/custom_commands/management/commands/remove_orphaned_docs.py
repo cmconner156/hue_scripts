a930e4fd8e2c49936e9103ec49a421a6b322093e
cconner <cconner@cloudera.com>
2019-04-24 10:27:44 -0400

    Updated stuff

--- lib/custom_commands/management/commands/remove_orphaned_docs.py
+++ lib/custom_commands/management/commands/remove_orphaned_docs.py
@@ -24,67 +24,68 @@ LOG = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    """
-    Handler for purging old Query History, Workflow documents and Session data
-    """
+  """
+  Handler for purging old Query History, Workflow documents and Session data
+  """
 
-    try:
-        from optparse import make_option
-        option_list = BaseCommand.option_list + (
-            make_option("--keep-days", help=_t("Number of days of history data to keep."),
-                    action="store",
-                    type=int,
-                    default=30),
-        )
+  try:
+    from optparse import make_option
+    option_list = BaseCommand.option_list + (
+      make_option("--keep-days", help=_t("Number of days of history data to keep."),
+          action="store",
+          type=int,
+          default=30),
+    )
 
-    except AttributeError, e:
-        baseoption_test = 'BaseCommand' in str(e) and 'option_list' in str(e)
-        if baseoption_test:
-            def add_arguments(self, parser):
-                parser.add_argument("--keep-days", help=_t("Number of days of history data to keep."),
-                    action="store",
-                    type=int,
-                    default=30)
-        else:
-            LOG.exception(str(e))
-            sys.exit(1)
+  except AttributeError, e:
+    baseoption_test = 'BaseCommand' in str(e) and 'option_list' in str(e)
+    if baseoption_test:
+      def add_arguments(self, parser):
+        parser.add_argument("--keep-days", help=_t("Number of days of history data to keep."),
+          action="store",
+          type=int,
+          default=30)
+    else:
+      LOG.exception(str(e))
+      sys.exit(1)
 
-    def handle(self, *args, **options):
+  def handle(self, *args, **options):
 
-        LOG.warn("HUE_CONF_DIR: %s" % os.environ['HUE_CONF_DIR'])
-        LOG.info("DB Engine: %s" % desktop.conf.DATABASE.ENGINE.get())
-        LOG.info("DB Name: %s" % desktop.conf.DATABASE.NAME.get())
-        LOG.info("DB User: %s" % desktop.conf.DATABASE.USER.get())
-        LOG.info("DB Host: %s" % desktop.conf.DATABASE.HOST.get())
-        LOG.info("DB Port: %s" % str(desktop.conf.DATABASE.PORT.get()))
-        LOG.info("Removing any orphaned docs")
+    LOG.warn("HUE_CONF_DIR: %s" % os.environ['HUE_CONF_DIR'])
+    LOG.info("DB Engine: %s" % desktop.conf.DATABASE.ENGINE.get())
+    LOG.info("DB Name: %s" % desktop.conf.DATABASE.NAME.get())
+    LOG.info("DB User: %s" % desktop.conf.DATABASE.USER.get())
+    LOG.info("DB Host: %s" % desktop.conf.DATABASE.HOST.get())
+    LOG.info("DB Port: %s" % str(desktop.conf.DATABASE.PORT.get()))
+    LOG.info("Removing any orphaned docs")
 
-        start = time.time()
+    start = time.time()
 
-        totalUsers = User.objects.filter().values_list("id", flat=True)
-        totalDocs = Document2.objects.exclude(owner_id__in=totalUsers)
-#        deleteDirs = Document2.objects.exclude(owner_id__in=totalUsers).filter(type__in('directory'))
-        docstorage_id = "docstorage" + str(uuid.uuid4())
-        docstorage = find_or_create_user(docstorage_id[:30])
+    totalUsers = User.objects.filter().values_list("id", flat=True)
+    totalDocs = Document2.objects.exclude(owner_id__in=totalUsers)
+#    deleteDirs = Document2.objects.exclude(owner_id__in=totalUsers).filter(type__in('directory'))
+    docstorage_id = "docstorage" + str(uuid.uuid4())
+    docstorage = find_or_create_user(docstorage_id[:30])
 
-        for doc in totalDocs:
-          if doc.type == "oozie-workflow2":
-            print "migrating workflow: %s : %s : %s : %s : to user: %s" % (doc.name, doc.type, doc.owner_id, doc.parent_directory, docstorage_id)
-            name = doc2.name
-#            doc2 = doc2.copy(name=name, owner=docstorage)
-
-        for doc in totalDocs:
-          if doc.type == "directory":
-            print "deleting doc: %s : %s : %s" % (doc.name, doc.type, doc.owner_id)
-#            doc.delete()
+    for doc in totalDocs:
+      if doc.type == "oozie-workflow2":
+      name = doc2.name
+      doc2 = doc2.copy(name=name, owner=docstorage)
+      print "migrating workflow: %s : %s : %s : %s : to user: %s" % (doc2.name, doc2.type, doc2.owner_id, doc2.parent_directory, docstorage_id)
 
 
+    for doc in totalDocs:
+      if doc.type == "directory":
+      print "deleting doc: %s : %s : %s" % (doc.name, doc.type, doc.owner_id)
+#      doc.delete()
 
 
 
-        end = time.time()
-        elapsed = (end - start)
-        LOG.debug("Total time elapsed (seconds): %.2f" % elapsed)
+
+
+    end = time.time()
+    elapsed = (end - start)
+    LOG.debug("Total time elapsed (seconds): %.2f" % elapsed)
 
 
 
