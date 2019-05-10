c2869d77d1662b02cf8e7de2952d0ce3dfbe9311
cconner <cconner@cloudera.com>
2019-05-10 15:33:38 -0400

    Fixed agent process dir

--- lib/cm_environment.py
+++ lib/cm_environment.py
@@ -14,21 +14,29 @@ class Configurator(object):
     """
     Collect environment from CM supervisor
     """
-    cm_config_file = '/etc/cloudera-scm-agent/config.ini'
-    cm_supervisor_dir = '/var/run/cloudera-scm-agent'
-    if os.path.isfile(cm_config_file):
-      import ConfigParser
-      from ConfigParser import NoOptionError
-      config = ConfigParser.RawConfigParser()
-      config.read(cm_config_file)
+    cm_agent_process = subprocess.Popen('ps -ef | grep "[c]m agent\|[c]mf-agent" | awk \'{print $2}\'', shell=True, stdout=subprocess.PIPE)
+    cm_agent_pid = cm_agent_process.communicate()[0].split('\n')[0]
+#    cm_config_file = '/etc/cloudera-scm-agent/config.ini'
+#    cm_supervisor_dir = '/var/run/cloudera-scm-agent'
+    if cm_agent_pid != '':
+#      import ConfigParser
+#      from ConfigParser import NoOptionError
+#      config = ConfigParser.RawConfigParser()
+#      config.read(cm_config_file)
 #     cm_supervisor_dir = config.get('General', 'agent_wide_credential_cache_location')
 
       try:
         supervisor_process = subprocess.Popen('ps -ef | grep [s]upervisord | awk \'{print $2}\'', shell=True, stdout=subprocess.PIPE)
         supervisor_pid = supervisor_process.communicate()[0].split('\n')[0]
         cm_supervisor_dir = os.path.realpath('/proc/%s/cwd' % supervisor_pid)
+        if supervisor_pid == '':
+          LOG.exception("This appears to be a CM enabled cluster and supervisord is not running")
+          LOG.exception("Make sure you are running as root and CM supervisord is running")
+          sys.exit(1)
       except Exception, e:
-        LOG.exception("Unable to get valid supervisord, make sure you are running as root and make sure the CM supervisor is running")    
+        LOG.exception("This appears to be a CM enabled cluster and supervisord is not running")
+        LOG.exception("Make sure you are running as root and CM supervisord is running")
+        sys.exit(1)
 
       #Parse CM supervisor include file for Hue and set env vars
       cm_supervisor_dir = cm_supervisor_dir + '/include'
@@ -56,7 +64,7 @@ class Configurator(object):
       if not envline == None:
         empty, environment = envline.split("environment=")
         for envvar in environment.split(","):
-          if "HADOOP_C" in envvar or "PARCEL" in envvar or "DESKTOP" in envvar or "ORACLE" in envvar or "LIBRARY" in envvar:
+          if "HADOOP_C" in envvar or "PARCEL" in envvar or "DESKTOP" in envvar or "ORACLE" in envvar or "LIBRARY" in envvar or "CMF" in envvar:
             envkey, envval = envvar.split("=")
             envval = envval.replace("'", "").rstrip()
             os.environ[envkey] = envval
@@ -66,28 +74,63 @@ class Configurator(object):
       else:
         parcel_dir = "/opt/cloudera/parcels"
 
-      oracle_instant_client = "%s/ORACLE_INSTANT_CLIENT" % parcel_dir
-      ld_library_path = "%s/instantclient_11_2" % oracle_instant_client
-      os.environ["ORACLE_HOME"] = ld_library_path
-      if 'LD_LIBRARY_PATH' in os.environ:
-        os.environ["LD_LIBRARY_PATH"] = "%s:%s" % (os.environ["LD_LIBRARY_PATH"], ld_library_path)
-      else:
-        os.environ["LD_LIBRARY_PATH"] = ld_library_path
+      cloudera_config_script = None
+      if os.path.isfile('/usr/lib64/cmf/service/common/cloudera-config.sh'):
+        cloudera_config_script = '/usr/lib64/cmf/service/common/cloudera-config.sh'
+      elif os.path.isfile('/opt/cloudera/cm-agent/service/common/cloudera-config.sh'):
+        cloudera_config_script = '/opt/cloudera/cm-agent/service/common/cloudera-config.sh'
 
-      sys.path.append(os.environ["LD_LIBRARY_PATH"])
+      JAVA_HOME = None
+      if cloudera_config_script is not None:
+        locate_java = subprocess.Popen(['bash', '-c', '. %s; locate_java_home' % cloudera_config_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        for line in iter(locate_java.stdout.readline,''):
+          if 'JAVA_HOME' in line:
+            JAVA_HOME = line.rstrip().split('=')[1]
+
+      if JAVA_HOME is not None:
+        os.environ["JAVA_HOME"] = JAVA_HOME
+
+      if "JAVA_HOME" not in os.environ:
+        print "JAVA_HOME must be set and can't be found, please set JAVA_HOME environment variable"
+        sys.exit(1)
+
+
+      oracle_check_process = subprocess.Popen('grep -i oracle %s/hue*ini' % os.environ["HUE_CONF_DIR"], shell=True, stdout=subprocess.PIPE)
+      oracle_check = oracle_check_process.communicate()[0]
+      if not oracle_check == '':
+        #Make sure we set Oracle Client if configured
+        if "LD_LIBRARY_PATH" not in os.environ.keys():
+          if "SCM_DEFINES_SCRIPTS" in os.environ.keys():
+            for scm_script in os.environ["SCM_DEFINES_SCRIPTS"].split(":"):
+              if "ORACLE" in scm_script:
+                if os.path.isfile(scm_script):
+                  oracle_source = subprocess.Popen(". %s; env" % scm_script, stdout=subprocess.PIPE, shell=True, executable="/bin/bash")
+                  for line in oracle_source.communicate()[0].splitlines():
+                    if "LD_LIBRARY_PATH" in line:
+                      var, oracle_ld_path = line.split("=")
+                      os.environ["LD_LIBRARY_PATH"] = oracle_ld_path
+
+        if "LD_LIBRARY_PATH" not in os.environ.keys():
+          print "LD_LIBRARY_PATH can't be found, if you are using ORACLE for your Hue database"
+          print "then it must be set, if not, you can ignore"
+          print "  export LD_LIBRARY_PATH=/path/to/instantclient"
+
+    else:
+      print "CM does not appear to be running on this server"
+      print "If this is a CM managed cluster make sure the agent and supervisor are running"
+      print "Running with /etc/hue/conf as the HUE_CONF_DIR"
+      os.environ["HUE_CONF_DIR"] = "/etc/hue/conf"
 
   def reload_with_cm_env(self):
     try:
       from django.db.backends.oracle.base import Oracle_datetime
     except:
-      if 'LD_LIBRARY_PATH' in os.environ and "ORACLE_HOME" in os.environ:
-        LOG.warn("We need to reload the process to include LD_LIBRARY_PATH and ORACLE_HOME")
+      os.environ["SKIP_RELOAD"] = True
+      if 'LD_LIBRARY_PATH' in os.environ:
+        LOG.warn("We need to reload the process to include LD_LIBRARY_PATH for Oracle backend")
         try:
           os.execv(sys.argv[0], sys.argv)
         except Exception, exc:
           LOG.warn('Failed re-exec:', exc)
           sys.exit(1)
-
-    LOG.warn("Successfully reloaded: LD_LIBRARY_PATH: %s" % os.environ['LD_LIBRARY_PATH'])
-    LOG.warn("Successfully reloaded: ORACLE_HOME: %s" % os.environ['ORACLE_HOME'])
 
