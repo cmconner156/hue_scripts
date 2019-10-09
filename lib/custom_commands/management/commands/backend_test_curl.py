15602f423cefa23f822c310a1e4669b911e6f2e8
cconner <cconner@cloudera.com>
2019-10-09 12:58:02 -0400

    fixed stuff

--- lib/custom_commands/management/commands/backend_test_curl.py
+++ lib/custom_commands/management/commands/backend_test_curl.py
@@ -35,7 +35,7 @@ if hdfs_config.SECURITY_ENABLED.get():
   klist_check = subprocess.Popen(klist_cmd, shell=True, stdout=subprocess.PIPE)
   klist_princ = klist_check.communicate()[0]
   if not 'hue/1' in klist_princ:
-    LOG.exception("klist failed, please contact support")
+    LOG.exception("klist failed, please contact support: %s" % klist_princ)
     sys.exit(1)
   LOG.info("klist_princ: %s" % klist_princ)
 #  klist_princ = klist_check.communicate()[0].split('\n')[0]
